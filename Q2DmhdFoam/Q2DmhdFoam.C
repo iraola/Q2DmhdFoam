8f97c1bfd181b2ea19db04a0d9d667564bb594d4
iraola <eduiraola@gmail.com>
2020-12-21 18:26:17 +0100

    Original tutorial setup

---
+++ Q2DmhdFoam/Q2DmhdFoam.C
@@ -0,0 +1,138 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.1
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Q2DmhdFoam
+
+Description
+    Transient solver for buoyancy-driven laminar flow of incompressible
+    Newtonian fluids using the Boussinesq Model
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "pisoControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+
+#   include "setRootCase.H"
+
+#   include "createTime.H"
+#   include "createMesh.H"
+
+    pisoControl piso(mesh);
+
+#   include "readTransportProperties.H"
+#   include "readGravitationalAcceleration.H"
+#   include "createFields.H"
+#   include "initContinuityErrs.H"
+#   include "createTimeControls.H"
+#   include "CourantNo.H"
+#   include "setInitialDeltaT.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+        
+#       include "readTimeControls.H"
+#       include "CourantNo.H"
+#       include "setDeltaT.H"
+
+        fvVectorMatrix UEqn
+        (
+            fvm::ddt(U)
+          + fvm::div(phi, U)
+          - fvm::laplacian(nu, U)
+         ==
+           -beta*(T - T0)*g
+           -fvm::Sp(mag(B)/tauHa, U)
+        );
+
+        solve(UEqn == -fvc::grad(p));
+
+        // --- PISO loop
+
+        while (piso.correct())
+        {
+            volScalarField rUA = 1.0/UEqn.A();
+
+            U = rUA*UEqn.H();
+            phi = (fvc::interpolate(U) & mesh.Sf())
+                + fvc::ddtPhiCorr(rUA, U, phi);
+
+            adjustPhi(phi, U, p);
+
+            while (piso.correctNonOrthogonal())
+            {
+                fvScalarMatrix pEqn
+                (
+                    fvm::laplacian(rUA, p) == fvc::div(phi)
+                );
+
+                pEqn.setReference(pRefCell, pRefValue);
+                pEqn.solve();
+
+                if (piso.finalNonOrthogonalIter())
+                {
+                    phi -= pEqn.flux();
+                }
+            }
+
+#           include "continuityErrs.H"
+
+            U -= rUA*fvc::grad(p);
+            U.correctBoundaryConditions();
+        }
+
+        // Solve energy equation
+        solve
+        (
+            fvm::ddt(T)
+          + fvm::div(phi, T)
+          - fvm::laplacian(DT, T)
+          - sourceT/(rho0*Cp)
+        );
+
+        rho = rho0*(scalar(1) - beta*(T - T0));
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return(0);
+}
+
+
+// ************************************************************************* //
---
+++ meshAndGo.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+import MHDutils as utils
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess as sp
+import os
+
+######################
+### INITIALIZATIONS
+######################
+# Get main parameters from input file
+param = np.loadtxt('MHDinput', unpack=True, skiprows=2)
+Ha = param[0]
+Re = param[1]
+Gr = param[2]
+
+# Define main tag dictionary
+tag_dict = {
+    'B'   : '?',
+    'Ux'  : '?',
+    'T0'  : 0.0,
+    'a'   : 0.15/2,
+    'b'   : 0.15/2,
+    'q0'  : 0,
+    'qWall' : 0,
+    'm'   : 1,
+    'Th'  : '?',
+    'Tc'  : '?',
+    'g'   : -9.81
+}
+
+# Update main dictionary with physical properties
+phys_dict = {
+    'rho0'   : 9720,
+    'nu'     : 1.54e-7,
+    'Cp'     : 189,
+    'k'      : 22.36,
+    'beta'   : 1.2e-4,
+    'sigma'  : 763000
+}
+tag_dict.update(phys_dict)
+
+# Define directory names
+base_dir = 'baseCaseTagawa'
+curr_dir = 'tagawa'
+# Create new case directory
+sp.call('cp -r ' + base_dir + ' ' + curr_dir, shell=True)
+
+######################
+### CASE CONDITIONS
+######################
+# Calculations from the parametrization of the problem and assign
+# them to the dictionaries
+B = Ha / (2*tag_dict['b']) * np.sqrt(tag_dict['nu'] * tag_dict['rho0'] /
+    tag_dict['sigma'])
+Ux = Re * tag_dict['nu'] / tag_dict['a']
+# Calculate delta_T from the input Grashof number
+delta_T = Gr * tag_dict['nu']**2 / (abs(tag_dict['g']) * tag_dict['beta']
+    * (2*tag_dict['a'])**3 )
+# Get hot and cold temperature for walls
+Th = delta_T/2
+Tc = -delta_T/2
+# Tag and print values
+tag_dict['B'] = B
+tag_dict['Ux'] = Ux
+tag_dict['Th'] = Th
+tag_dict['Tc'] = Tc
+print('Magnetic field B = {} for Ha = {}'.format(tag_dict['B'],Ha))
+print('Mean velocity U = {} for Re = {}'.format(tag_dict['Ux'],Re))
+print('Temperature difference is {} for Grashof = {}'.format(delta_T,Gr))
+
+######################
+### MESH
+######################
+
+l_side = tag_dict['a']/np.sqrt(Ha)
+N_bl = 25
+G_bl, _, cmax = utils.lenC2CN(l_side, N_bl)
+
+print('Ha = {}'.format(Ha))
+print('SIDE BOUNDARY LAYER')
+print('side boundary length = {}'.format(l_side))
+print('G = {}'.format(G_bl))
+print('cmax = {}'.format(cmax))
+
+# This is HALF the length of the bulk
+l_bulk = tag_dict['a'] - l_side
+N_bulk, G_bulk, _ = utils.lenCminC2C(l_bulk, cmax)
+print('HALF BULK')
+print('bulk half length = {}'.format(l_bulk))
+print('N = {}'.format(N_bulk))
+print('G = {}'.format(G_bulk))
+
+mesh_dict = {
+    'Lx'        : 200*tag_dict['a'],
+    'LxHalf'    : 200*tag_dict['a'] / 2,
+    'LyBulk'    : tag_dict['a'],
+    'LyBL'      : tag_dict['a']-l_side,
+    'LyNegBulk' : -tag_dict['a'],
+    'LyNegBL'   : -(tag_dict['a']-l_side),
+    'Nx'        : 100,
+    'NyBulk'    : N_bulk,
+    'NyBL'      : N_bl,
+    'GyBulk'    : G_bulk,
+    'GyBulkInv' : 1/G_bulk,
+    'GyBL'      : G_bl,
+    'GyBLinv'   : 1/G_bl
+}
+# Combine the mesh dictionary with the previous dictionary
+tag_dict.update(mesh_dict)
+# Replace all tags
+utils.replaceTags(tag_dict, curr_dir)
+
+
+######################
+### RUN CASE
+######################
+os.chdir(curr_dir)
+sp.call("blockMesh > log.blockMesh", shell=True)
+sp.call("Q2DmhdFoam > log.Q2DmhdFoam", shell=True)
+sp.call("sample > log.sample", shell=True)
+
+######################
+### PLOT & VALIDATION
+######################
+# Analytical solution
+T = np.linspace(Th,Tc)
+theta = (T - 1/2*(Th + Tc))/(Th-Tc)
+y_analytic = np.linspace(-tag_dict['a'],tag_dict['a'])
+W_analytic = Gr/(2*Ha) * theta
+fig, ax = plt.subplots(figsize=(12,12))
+ax.plot(y_analytic[10:-10]/(2*tag_dict['a']), W_analytic[10:-10], label='Bulk analytical solution')
+
+# Q2DmhdFoam solution
+postProcess_dir = 'postProcessing/sets/'
+latestTime = utils.getLatestTime(postProcess_dir)
+raw_data_file = postProcess_dir + latestTime + '/line_centreProfile_U.xy'
+y, Ux, Uy, Uz = np.loadtxt(raw_data_file, unpack=True)
+y = y - tag_dict['a']
+W = Ux / (tag_dict['nu'] / (2*tag_dict['a']))
+ax.plot(y/(2*tag_dict['a']), W, label='Q2DmhdFoam')
+
+# Plot config
+ax.set_xlabel('Dimensionless length, $X=x/l$')
+ax.set_ylabel('Dimensionless velocity, $W$')
+ax.legend(loc='upper right')
+plt.show()
+fig.savefig('tagawaVal.eps',format='eps',dpi=1000)
