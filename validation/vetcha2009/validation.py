285dbfdee6d3ed8f99d7712b1bd536b20233692d
iraola <eduiraola@gmail.com>
2021-04-21 19:45:25 +0200

    Copy poiseuille case as starting point for new vetcha validation case

---
+++ validation/vetcha2009/validation.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+# import custom modules
+sys.path.insert(1, '../../python')
+from meshAndGo import meshAndGo
+
+# Setup case
+tag_dict = {
+    'B'   : '?',
+    'Ux'  : '?',
+    'T0'  : 0.0,
+    'a'   : 0.15/2,
+    'b'   : 0.15/2,
+    'q0'  : 0,
+    'qWall' : 0,
+    'm'   : 1,
+    'Th'  : '?',
+    'Tc'  : '?',
+    'g'   : -9.81
+}
+phys_dict = {
+    'rho0'   : 9720,
+    'nu'     : 1.54e-7,
+    'Cp'     : 189,
+    'k'      : 22.36,
+    'beta'   : 1.2e-4,
+    'sigma'  : 763000
+}
+
+# Run case
+meshAndGo(Ha=100, Re=100, Gr=0, Nx=1, Lx=0.1,
+    tag_dict=tag_dict, phys_dict=phys_dict)
+
+# Solution for the Poiseuille Flow between two infinite plates
+h = 2*tag_dict['a']
+y = np.linspace(0, h)
+deltaP = 1
+mu = phys_dict['rho0']*phys_dict['nu']
+u = deltaP/(2*mu) * y * (h-y)
+plt.plot(y,u)
+plt.show()
