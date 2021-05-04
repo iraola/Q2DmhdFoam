ccab1bec6b0bf8c586f7fae7e5dc97763446db63
iraola <eduiraola@gmail.com>
2021-05-04 21:00:43 +0200

    Modify general meshAndGo script to include volumetric heat
    The user can now select how the Grashof number is
    calculated: with volumetric heat (q and m) or with
    wall fixed temperatures (Tc and Th).
    
    Other changes have been made to avoid going through
    Tagawa calculations all the times.
    
    Also, the user can now input mesh parameters to overwrite
    the original ones

--- python/meshAndGo.py
+++ python/meshAndGo.py
@@ -6,15 +6,13 @@ import matplotlib.pyplot as plt
 import subprocess as sp
 import os
 
-def meshAndGo(Ha, Re, Gr, Nx=100, Lx=20, write=False, plot=False, **kwargs):
+def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False, **kwargs):
     '''
     Setup, mesh, run and plot results for a Q2DmhdFOAM case
 
     Parameters
     ----------
-    Nx : Number of cells in the direction of the flow
-    Lx : Proportion of the length in the direction of the flow with respect to
-        the half-length 'a' of the channel
+
     '''
 
     ######################
@@ -26,36 +24,40 @@ def meshAndGo(Ha, Re, Gr, Nx=100, Lx=20, write=False, plot=False, **kwargs):
     # Re = param[1]
     # Gr = param[2]
 
-    # Optional arguments
+    # Define main tag dictionary
+    tag_dict = {
+        'B'   : '?',
+        'Ux'  : '?',
+        'T0'  : 0.0,
+        'a'   : 0.15/2,
+        'b'   : 0.15/2,
+        'q0'  : 0,
+        'qWall' : 0,
+        'm'   : 1,
+        'Th'  : 0,
+        'Tc'  : 0,
+        'g'   : -9.81
+    }
+    # Update main dictionary with physical properties
+    phys_dict = {
+        'rho0'   : 9720,
+        'nu'     : 1.54e-7,
+        'Cp'     : 189,
+        'k'      : 22.36,
+        'beta'   : 1.2e-4,
+        'sigma'  : 763000
+    }
+    # Optional arguments for changing default dicts using kwargs
+    #   NOTE: if the user provides parameters that already exist in the dicts,
+    #   they will be overwritten. The rest will remain the same.
+    #   This is very useful if you want to use all defaults except a couple of
+    #   variables
     if 'tag_dict' in kwargs:
-        tag_dict = kwargs['tag_dict']
-    else:
-        # Define main tag dictionary
-        tag_dict = {
-            'B'   : '?',
-            'Ux'  : '?',
-            'T0'  : 0.0,
-            'a'   : 0.15/2,
-            'b'   : 0.15/2,
-            'q0'  : 0,
-            'qWall' : 0,
-            'm'   : 1,
-            'Th'  : '?',
-            'Tc'  : '?',
-            'g'   : -9.81
-        }
+        tag_dict.update(kwargs['tag_dict'])
     if 'phys_dict' in kwargs:
-        phys_dict = kwargs['phys_dict']
-    else:
-        # Update main dictionary with physical properties
-        phys_dict = {
-            'rho0'   : 9720,
-            'nu'     : 1.54e-7,
-            'Cp'     : 189,
-            'k'      : 22.36,
-            'beta'   : 1.2e-4,
-            'sigma'  : 763000
-        }
+        phys_dict.update(kwargs['phys_dict'])
+
+    # Add all tags into same dict
     tag_dict.update(phys_dict)
 
     # Define directory names
@@ -71,20 +73,35 @@ def meshAndGo(Ha, Re, Gr, Nx=100, Lx=20, write=False, plot=False, **kwargs):
     ######################
     # Calculations from the parametrization of the problem and assign
     # them to the dictionaries
+    # FROM HARTMANN:
     B = Ha / (2*tag_dict['a']) * np.sqrt(tag_dict['nu'] * tag_dict['rho0'] /
         tag_dict['sigma'])
+
+    # FROM REYNOLDS:
     Ux = Re * tag_dict['nu'] / tag_dict['a']
-    # Calculate delta_T from the input Grashof number
-    delta_T = Gr * tag_dict['nu']**2 / (abs(tag_dict['g']) * tag_dict['beta']
-        * (2*tag_dict['a'])**3 )
-    # Get hot and cold temperature for walls
-    Th = delta_T/2
-    Tc = -delta_T/2
+
+    # FROM GRASHOF:
+    if volumetric_heat:
+        # Calculate exponential deposition q0 (tepot case)
+        delta_T = Gr * tag_dict['nu']**2 / (tag_dict['g']*tag_dict['beta']*
+                    tag_dict['a']**3)
+        S0 = delta_T * tag_dict['k'] / (tag_dict['a']**2)
+        q0 = S0 * (2 * tag_dict['a'] * tag_dict['m']) / (1
+            - np.exp(- 2 * tag_dict['a'] * tag_dict['m']))
+        tag_dict['q0'] = q0
+    else:
+        # Calculate delta_T from the input Grashof number (Tagawa's case)
+        delta_T = Gr * tag_dict['nu']**2 / (abs(tag_dict['g']) * tag_dict['beta']
+                * (2*tag_dict['a'])**3 )
+        # Get hot and cold temperature for walls
+        Th = delta_T/2
+        Tc = -delta_T/2
+        tag_dict['Th'] = Th
+        tag_dict['Tc'] = Tc
+
     # Tag and print values
     tag_dict['B'] = B
     tag_dict['Ux'] = Ux
-    tag_dict['Th'] = Th
-    tag_dict['Tc'] = Tc
     print('Magnetic field B = {} for Ha = {}'.format(tag_dict['B'],Ha))
     print('Mean velocity U = {} for Re = {}'.format(tag_dict['Ux'],Re))
     print('Temperature difference is {} for Grashof = {}'.format(delta_T,Gr))
@@ -93,6 +110,11 @@ def meshAndGo(Ha, Re, Gr, Nx=100, Lx=20, write=False, plot=False, **kwargs):
     ### MESH
     ######################
 
+    # Default constants (can be ovewritten by user's mesh_dict)
+    Nx = 100    # Nx : Number of cells in the direction of the flow
+    Lx = 20     # Lx : Proportion of the length in the direction of the flow
+                # with respect to the half-length 'a' of the channel
+    # Mesh MHD calculations
     l_side = tag_dict['a']/np.sqrt(Ha)
     N_bl = 25
     G_bl, _, cmax = utils.lenC2CN(l_side, N_bl)
@@ -126,6 +148,11 @@ def meshAndGo(Ha, Re, Gr, Nx=100, Lx=20, write=False, plot=False, **kwargs):
         'GyBL'      : G_bl,
         'GyBLinv'   : 1/G_bl
     }
+
+    # Again, overwrite values if the user provides them:
+    if 'mesh_dict' in kwargs:
+        mesh_dict.update(kwargs['mesh_dict'])
+
     # Combine the mesh dictionary with the previous dictionary
     tag_dict.update(mesh_dict)
     # Replace all tags
@@ -144,30 +171,47 @@ def meshAndGo(Ha, Re, Gr, Nx=100, Lx=20, write=False, plot=False, **kwargs):
     ### PLOT & VALIDATION
     ######################
     # Analytical solution
-    T = np.linspace(Th,Tc)
-    theta = (T - 1/2*(Th + Tc))/(Th-Tc)
-    y_analytic = np.linspace(-tag_dict['a'],tag_dict['a'])
-    W_analytic = Gr/(2*Ha) * theta
 
-    # Q2DmhdFoam solution
-    postProcess_dir = 'postProcessing/sets/'
-    latestTime = utils.getLatestTime(postProcess_dir)
-    raw_data_file = postProcess_dir + latestTime + '/line_centreProfile_U.xy'
-    y, Ux, Uy, Uz = np.loadtxt(raw_data_file, unpack=True)
-    y = y - tag_dict['a']
-    W = Ux / (tag_dict['nu'] / (2*tag_dict['a']))
-    # Print max velocity
-    print('The Q2D maximum velocity is : ', np.max(W))
+    if plot and not volumetric_heat:
+        T = np.linspace(tag_dict['Th'],tag_dict['Tc'])
+        theta = (T - 1/2*(tag_dict['Th'] + tag_dict['Tc'])) / (
+                tag_dict['Th']-tag_dict['Tc'])
+        y_analytic = np.linspace(-tag_dict['a'],tag_dict['a'])
+        W_analytic = Gr/(2*Ha) * theta
 
-    # Plot derivative of the Q2D solution
-    # Note: Use dimensionless Y. Y = y / l
-    y_plot = np.zeros(len(y)-1)
-    Y = y / (2*tag_dict['a'])
-    dW = np.zeros(len(W)-1)
-    for i in range(len(W) - 1):
-        dW[i] = (W[i+1] - W[i]) / (Y[i+1] - Y[i])
-        y_plot[i] = (Y[i] + Y[i+1])/2
+        # Q2DmhdFoam solution
+        postProcess_dir = 'postProcessing/sets/'
+        latestTime = utils.getLatestTime(postProcess_dir)
+        raw_data_file = postProcess_dir + latestTime + '/line_centreProfile_U.xy'
+        y, Ux, Uy, Uz = np.loadtxt(raw_data_file, unpack=True)
+        y = y - tag_dict['a']
+        W = Ux / (tag_dict['nu'] / (2*tag_dict['a']))
+        # Print max velocity
+        print('The Q2D maximum velocity is : ', np.max(W))
 
+        # Plot derivative of the Q2D solution
+        # Note: Use dimensionless Y. Y = y / l
+        y_plot = np.zeros(len(y)-1)
+        Y = y / (2*tag_dict['a'])
+        dW = np.zeros(len(W)-1)
+        for i in range(len(W) - 1):
+            dW[i] = (W[i+1] - W[i]) / (Y[i+1] - Y[i])
+            y_plot[i] = (Y[i] + Y[i+1])/2
+
+        # PLOT
+        fig, ax = plt.subplots(figsize=(12,12))
+        ax.plot(y_analytic[10:-10]/(2*tag_dict['a']), W_analytic[10:-10],
+            label='Bulk analytical solution')
+        ax.plot(y/(2*tag_dict['a']), W, label='Q2DmhdFoam')
+        fig1, ax1 = plt.subplots(figsize=(12,12))
+        ax1.plot(y_plot, -dW, label='Q2D dW/dY')
+        ax1.set_ylim(bottom=0)
+        # Plot config
+        ax.set_xlabel('Dimensionless length, $X=x/l$')
+        ax.set_ylabel('Dimensionless velocity, $W$')
+        ax.legend(loc='upper right')
+        fig.savefig('results.eps',format='eps',dpi=1000)
+        plt.show()
 
     if write:
         # Write out tagawa values
@@ -185,23 +229,5 @@ def meshAndGo(Ha, Re, Gr, Nx=100, Lx=20, write=False, plot=False, **kwargs):
         with open(out_file, 'w') as file:
             file.write(text)
 
-    # PLOT
-    if plot == True:
-        fig, ax = plt.subplots(figsize=(12,12))
-        ax.plot(y_analytic[10:-10]/(2*tag_dict['a']), W_analytic[10:-10],
-            label='Bulk analytical solution')
-        ax.plot(y/(2*tag_dict['a']), W, label='Q2DmhdFoam')
-        fig1, ax1 = plt.subplots(figsize=(12,12))
-        ax1.plot(y_plot, -dW, label='Q2D dW/dY')
-        ax1.set_ylim(bottom=0)
-        # Plot config
-        ax.set_xlabel('Dimensionless length, $X=x/l$')
-        ax.set_ylabel('Dimensionless velocity, $W$')
-        ax.legend(loc='upper right')
-        fig.savefig('results.eps',format='eps',dpi=1000)
-        plt.show()
-
     # Return to original directory
     os.chdir('..')
-
-    return np.max(W), np.max(W_analytic)
