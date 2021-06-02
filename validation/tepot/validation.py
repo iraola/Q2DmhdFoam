60d9a2c2df1d45875b61f7426f003ff633fb0e0b
iraola <eduiraola@gmail.com>
2021-06-02 21:54:07 +0200

    Update tepot validation scripts with code improvements

--- validation/tepot/validation.py
+++ validation/tepot/validation.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import os
 import subprocess as sp
 import pdb
 # import custom modules
@@ -11,7 +12,7 @@ from meshAndGo import meshAndGo
 from MHDutils import getLatestTime
 
 ### INITIALIZATIONS
-validation_file = 'vetcha2009_Ha50_Re1e4.csv'
+validation_dir = 'samples'
 postprocess_dir = 'case/postProcessing/sets/'
 postprocess_file = 'line_centreProfile_U.xy'
 # Case setup tags
@@ -38,39 +39,49 @@ mesh_dict = {       # SETUP FOR CYCLIC ONE-CELL CASE
     'Lx'    : 0.1,
     'LxHalf': 0.1/2,
     'Nx'    : 1}
-Grashofs = [5e6, 5e7, 1e8]
-Re = 1e4
-Ha = 3000
-# Load validation dataset
-names = ['x_Gr5e6','y_Gr5e6','x_Gr5e7','y_Gr5e7','x_Gr1e8','y_Gr1e8']
-val_data = pd.read_csv(validation_file, skiprows=2, names=names)
-val_data.head()
+a = tag_dict['a']   # take 'a' length that we'll use later
+
 # Initialize plot
 fig, ax = plt.subplots(figsize=(12,6))
+
+def getConditions(filename):
+    ''' Get Ha, Re, Gr from the name of a file as real numbers'''
+    my_list = filename.split('_')
+    Ha = float(my_list[1])
+    Re = float(my_list[2])
+    Gr = float(my_list[3])
+    return Ha, Re, Gr
 
 ### LOOP
 # Remove old directories present
 sp.call("rm -r -f case_*", shell=True)
-for Gr in Grashofs:
-    i = Grashofs.index(Gr)
+for file in os.listdir(validation_dir):
+    # Get dimless numbers from the filenames
+    Ha, Re, Gr = getConditions(file)
+    print(Ha, Re, Gr)
+
     # Run case with the meshAndGo module
     meshAndGo(Ha, Re, Gr,
         mesh_dict=mesh_dict, tag_dict=tag_dict, phys_dict=phys_dict)
     # Store the simulated case in other directory
-    sp.call("cp -r case case_" + str(Gr), shell=True)
+    sp.call("cp -r case case_" + str(Ha) + "_" + str(Gr), shell=True)
+
     # Get latest time directory name and load simulation data
     latest_time = getLatestTime(postprocess_dir)
     filename = postprocess_dir + latest_time + '/' + postprocess_file
-    # Plot sim. data
+    # Plot
     z, U, _, _ = np.loadtxt(filename, unpack= True)
+    z_val, _, U_val = np.loadtxt(validation_dir + '/' + file, unpack= True)
     # Normalize data
     U /= U.mean()
-    z = (z - tag_dict['a']) / tag_dict['a']
-    # Plot simulationdata
-    ax.plot(z, U, label='Q2D Gr='+str(Gr))
+    z = (z - a) / a
+    U_val /= U_val.mean()
+    U_val = np.flip(U_val)
+    z_val = (z_val - 2*a) / (2*a)
+    # Plot simulation data
+    ax.plot(z, U, label='Q2D Ha=' + str(Ha) + ' Gr='+str(Gr))
     # Plot validation data
-    ax.plot(val_data[val_data.columns[2*i]],
-            val_data[val_data.columns[2*i+1]], label='Vetcha Gr='+str(Gr))
+    ax.plot(z_val, U_val, label='tepot Ha=' + str(Ha) + ' Gr='+str(Gr))
 
 
 ax.legend(loc='best')
