460c5dc4a4bf2afa5e9c8a720ded73894f40c350
iraola <eduiraola@gmail.com>
2021-07-31 18:30:47 +0200

    Upgrade meshAndGo: c2c improvement and style

--- python/meshAndGo.py
+++ python/meshAndGo.py
@@ -62,6 +62,12 @@ def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False,
     # Add all tags into same dict
     tag_dict.update(phys_dict)
 
+    # Get dict variables for easy handling
+    a = tag_dict['a'];      b = tag_dict['b']
+    nu = tag_dict['nu'];    rho = tag_dict['rho0'];     sigma = tag_dict['sigma']
+    g = tag_dict['g'];      beta = tag_dict['beta'];    k = tag_dict['k']
+    m = tag_dict['m']
+
     # Define directory names
     base_dir = 'baseCase'
     curr_dir = 'case'
@@ -76,25 +82,19 @@ def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False,
     # Calculations from the parametrization of the problem and assign
     # them to the dictionaries
     # FROM HARTMANN:
-    B = Ha / (2*tag_dict['a']) * np.sqrt(tag_dict['nu'] * tag_dict['rho0'] /
-        tag_dict['sigma'])
+    B = Ha / (2 * a) * np.sqrt(nu * rho / sigma)
 
     # FROM REYNOLDS:
-    Ux = Re * tag_dict['nu'] / tag_dict['a']
+    Ux = Re * nu / a
 
     # FROM GRASHOF:
+    delta_T = Gr * nu**2 / (abs(g) * beta * (2 * a)**3)
     if volumetric_heat:
         # Calculate exponential deposition q0 (tepot case)
-        delta_T = Gr * tag_dict['nu']**2 / (abs(tag_dict['g'])*tag_dict['beta']*
-                    tag_dict['a']**3)
-        S0 = delta_T * tag_dict['k'] / (tag_dict['a']**2)
-        q0 = S0 * (2 * tag_dict['a'] * tag_dict['m']) / (1
-            - np.exp(- 2 * tag_dict['a'] * tag_dict['m']))
+        S0 = delta_T * k / (a**2)
+        q0 = S0 * (2 * a * m) / (1 - np.exp(- 2 * a * m))
         tag_dict['q0'] = q0
     else:
-        # Calculate delta_T from the input Grashof number (Tagawa's case)
-        delta_T = Gr * tag_dict['nu']**2 / (abs(tag_dict['g']) * tag_dict['beta']
-                * (2*tag_dict['a'])**3 )
         # Get hot and cold temperature for walls
         Th = delta_T/2
         Tc = -delta_T/2
@@ -104,8 +104,8 @@ def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False,
     # Tag and print values
     tag_dict['B'] = B
     tag_dict['Ux'] = Ux
-    print('Magnetic field is B = {} for Ha = {}'.format(tag_dict['B'], Ha))
-    print('Mean velocity is U = {} for Re = {}'.format(tag_dict['Ux'], Re))
+    print('Magnetic field is B = {} for Ha = {}'.format(B, Ha))
+    print('Mean velocity is U = {} for Re = {}'.format(Ux, Re))
     print('Temperature difference is {} for Grashof = {}'.format(delta_T, Gr))
 
     ######################
@@ -116,16 +116,27 @@ def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False,
     Nx = 100    # Nx : Number of cells in the direction of the flow
     Lx = 20     # Lx : Proportion of the length in the direction of the flow
                 # with respect to the half-length 'a' of the channel
+    # Check if user provides cell-to-cell ratios for meshing
+    if 'mesh_dict' in kwargs:
+        try:
+            c2c_bl = kwargs['mesh_dict']['c2c_bl']
+            c2c_bulk = kwargs['mesh_dict']['c2c_bulk']
+        except:
+            print('Please provide "c2c_bl" and "c2c_bulk" in "mesh_dict"')
+    else:
+        c2c_bl = 1.05
+        c2c_bulk = 1.1
+
     # Mesh MHD calculations
     if Ha == 0:
-        l_side = tag_dict['a']/10
+        l_side = 3 * a / 10
     else:
-        l_side = tag_dict['a']/np.sqrt(Ha)
+        l_side = 3 * a / np.sqrt(Ha)
     N_bl = 25
-    G_bl, _, cmax = utils.lenC2CN(l_side, N_bl)
+    G_bl, _, cmax = utils.lenC2CN(l_side, N_bl, c2c_bl)
     # This is HALF the length of the bulk
-    l_bulk = tag_dict['a'] - l_side
-    N_bulk, G_bulk, _ = utils.lenCminC2C(l_bulk, cmax)
+    l_bulk = a - l_side
+    N_bulk, G_bulk, _ = utils.lenCminC2C(l_bulk, cmax, c2c_bulk)
     if verbose:
         print('Ha = {}'.format(Ha))
         print('SIDE BOUNDARY LAYER')
@@ -138,12 +149,12 @@ def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False,
         print('G = {}'.format(G_bulk))
 
     mesh_dict = {
-        'Lx'        : Lx*tag_dict['a'],
-        'LxHalf'    : Lx*tag_dict['a'] / 2,
-        'LyBulk'    : tag_dict['a'],
-        'LyBL'      : tag_dict['a']-l_side,
-        'LyNegBulk' : -tag_dict['a'],
-        'LyNegBL'   : -(tag_dict['a']-l_side),
+        'Lx'        : Lx*a,
+        'LxHalf'    : Lx*a / 2,
+        'LyBulk'    : a,
+        'LyBL'      : a-l_side,
+        'LyNegBulk' : -a,
+        'LyNegBL'   : -(a-l_side),
         'Nx'        : Nx,
         'NyBulk'    : N_bulk,
         'NyBL'      : N_bl,
@@ -177,26 +188,25 @@ def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False,
     # Analytical solution
 
     if plot and not volumetric_heat:
-        T = np.linspace(tag_dict['Th'],tag_dict['Tc'])
-        theta = (T - 1/2*(tag_dict['Th'] + tag_dict['Tc'])) / (
-                tag_dict['Th']-tag_dict['Tc'])
-        y_analytic = np.linspace(-tag_dict['a'],tag_dict['a'])
-        W_analytic = Gr/(2*Ha) * theta
+        T = np.linspace(Th,Tc)
+        theta = (T - 1/2 * (Th + Tc)) / (Th - Tc)
+        y_analytic = np.linspace(-a,a)
+        W_analytic = Gr / (2 * Ha) * theta
 
         # Q2DmhdFoam solution
         postProcess_dir = 'postProcessing/sets/'
         latestTime = utils.getLatestTime(postProcess_dir)
         raw_data_file = postProcess_dir + latestTime + '/line_centreProfile_U.xy'
         y, Ux, Uy, Uz = np.loadtxt(raw_data_file, unpack=True)
-        y = y - tag_dict['a']
-        W = Ux / (tag_dict['nu'] / (2*tag_dict['a']))
+        y = y - a
+        W = Ux / (nu / (2*a))
         # Print max velocity
         print('The Q2D maximum velocity is : ', np.max(W))
 
         # Plot derivative of the Q2D solution
         # Note: Use dimensionless Y. Y = y / l
         y_plot = np.zeros(len(y)-1)
-        Y = y / (2*tag_dict['a'])
+        Y = y / (2*a)
         dW = np.zeros(len(W)-1)
         for i in range(len(W) - 1):
             dW[i] = (W[i+1] - W[i]) / (Y[i+1] - Y[i])
@@ -204,9 +214,9 @@ def meshAndGo(Ha, Re, Gr, volumetric_heat=True, write=False, plot=False,
 
         # PLOT
         fig, ax = plt.subplots(figsize=(12,12))
-        ax.plot(y_analytic[10:-10]/(2*tag_dict['a']), W_analytic[10:-10],
+        ax.plot(y_analytic[10:-10]/(2*a), W_analytic[10:-10],
             label='Bulk analytical solution')
-        ax.plot(y/(2*tag_dict['a']), W, label='Q2DmhdFoam')
+        ax.plot(y/(2*a), W, label='Q2DmhdFoam')
         fig1, ax1 = plt.subplots(figsize=(12,12))
         ax1.plot(y_plot, -dW, label='Q2D dW/dY')
         ax1.set_ylim(bottom=0)
